2420ba337d47dff72b7f462f49e485026d9ba7f0
sy stroud <stroudafk@gmail.com>
2025-05-30 21:05:59 -0400

    nvim | label keymap mode sections

--- nvim/dot-config/nvim/lua/core/keymaps.lua
+++ nvim/dot-config/nvim/lua/core/keymaps.lua
@@ -4,6 +4,7 @@ vim.g.mapleader=" "
 -- "ayy to name a register to write to, "Ayy (capital register name) to write to the named register. does the same as above but will overwrite the buffer if you mistype a lowercase 
 -- "ap to paste/put
 
+--------------------NORMAL MODE--------------------
 vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', {desc = 'Toggle NeoTree'})
 
@@ -24,7 +25,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Insert Mode
+--------------------INSERT MODE--------------------
 -- easy Esc on normie boards
 vim.keymap.set("i", "jj", "<Esc")
 
@@ -32,7 +33,7 @@ vim.keymap.set("i", "jj", "<Esc")
 vim.keymap.set('i', '<A-Up>', '<Esc><cmd>m-2<CR>==gi', { desc = 'Move line up'})
 vim.keymap.set('i', '<A-Down>', '<Esc><cmd>m+<CR>==gi', { desc = 'Move line down'})
 
--- move line above/below and match indent
+--------------------VISUAL MODE--------------------
 vim.keymap.set('v', '<A-Up>', "<cmd>m '<-2<CR>gv=gv", { desc = 'Move line up'})
 vim.keymap.set('v', '<A-Down>', "<cmd>m '>+1<CR>gv=gv", { desc = 'Move line down'})
 
